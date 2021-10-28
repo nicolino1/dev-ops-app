d9518d1199334d2e289a8bbe1ac78cfb41f93096
jfiora <sillonpeludo@gmail.com>
2021-10-28 13:30:16 -0300

    get and post

--- dev-ops-app/src/App.js
+++ dev-ops-app/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/card/card'
 
 function App() {
   return (
     <div className="App">
-      <Card title="hola" description= "como va"/>
+      <Card/>
     </div>
   );
 }
--- dev-ops-app/src/components/card/card.js
+++ dev-ops-app/src/components/card/card.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './card.scss';
+import Axios from 'axios';
 
-const Card = (props) => {
-    const  {title, description} = props;
+const Card = () => {
+    const [books, setbooks] = useState([]);
+    const url = "http://localhost:4000/api/books";
+    const fetchbooks = async () => {
+        const { data } = await Axios.get(url);
+        const books = data;
+        setbooks(books);
+    };
+
+    useEffect(() => {
+        fetchbooks();
+      }, []);
 
     return (
         <div className="card">
-            <img src="img_avatar.png" alt="Avatar"/>
-            <div>
-                <h4>{title}</h4>
-                <p>{description}</p>
-            </div>
+            {books.map((book) => (
+                <div key={book._id}>
+                    <h4>{book.title}</h4>
+                    <p>{book.author}</p>
+                </div>
+            ))}
         </div>
     )
 
